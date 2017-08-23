9c5cc6d39cba585f59382377850fbee87854addf
Jason Koo <jkoo@starbucks.com>
2017-08-23 01:20:58 -0700

    adding chef

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,12 @@ echo "pulling latest version of the code"
 ssh root@40.78.97.72 'docker pull coolkoo/nginx_image:latest'
 
 echo "starting the new version"
-ssh root@40.78.97.72 'docker run -d --restart=always --name nginx -p 80:80 coolkoo/nginx_image:latest'
+ssh root@40.78.97.72 'docker run -d --restart=always --name nginx -p 8081:8081 coolkoo/nginx_image:latest'
 
 echo "success!"
+
+
+echo "deploying HAproxy with Chef"
+ssh root@40.78.97.72 'chef-client'
 
 exit 0
