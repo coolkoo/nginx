69cf5dc128ff0e9c5ceb92153497491109e4b2b5
Jason Koo <jkoo@starbucks.com>
2017-08-23 01:59:42 -0700

    fixing ipaddress

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 echo "stopping running application"
-ssh root@40.78.97.72 'docker stop $(docker ps -a -q)'
-ssh root@40.78.97.72 'docker rm $(docker ps -a -q)'
+ssh root@distelli.cloudapp.net 'docker stop $(docker ps -a -q)'
+ssh root@distelli.cloudapp.net 'docker rm $(docker ps -a -q)'
 
 echo "pulling latest version of the code"
-ssh root@40.78.97.72 'docker pull coolkoo/nginx_image:latest'
+ssh root@distelli.cloudapp.net 'docker pull coolkoo/nginx_image:latest'
 
 echo "starting the new version"
-ssh root@40.78.97.72 'docker run -d --restart=always --name nginx -p 8081:80 coolkoo/nginx_image:latest'
+ssh root@distelli.cloudapp.net 'docker run -d --restart=always --name nginx -p 8081:80 coolkoo/nginx_image:latest'
 
 echo "success!"
 
 
 echo "deploying HAproxy with Chef"
-ssh root@40.78.97.72 'chef-client'
+ssh root@distelli.cloudapp.net 'chef-client'
 
 exit 0
